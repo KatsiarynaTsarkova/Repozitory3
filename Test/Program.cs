a8d25373212d934603fe78d41360292cb8fb58b4
Kate <katya_tsarkov@mail.ru>
2022-09-08 21:10:45 +0300

    Test

---
+++ Test/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Ведите цифру, обозначающую день недели");
+int week = int.Parse(Console.ReadLine());
+if (week== 6  || week==7)
+{
+    Console.WriteLine("Выходной");
+}
+else
+{
+     Console.WriteLine("Будний");
+}
