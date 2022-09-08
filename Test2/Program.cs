4ac1219961b55afd5c0c01e0a3be64f22c776fd0
Kate <katya_tsarkov@mail.ru>
2022-09-08 20:42:20 +0300

    Test2

---
+++ Test2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите трехзначное число");
+string treeNum = Console.ReadLine();
+if(treeNum.Length != 3)
+{
+   Console.WriteLine("Третьей цифры нет"); 
+}
+else
+{
+    //int Number = int.Parse(treeNum);
+    Console.WriteLine("Третья цифра" + (treeNum[2]));
+}
